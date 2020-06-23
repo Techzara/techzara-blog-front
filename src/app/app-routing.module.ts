b02349656ff191a64da2db194a1684ee1ce67572
Mathias <adriela_ratefiarivony@hotmail.com>
2020-06-23 21:20:12 +0300

    ajout login et inscription

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {InscriptionComponent} from "./components/inscription/inscription.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  { // quand le path est home , charger en asynchrone le module Homemodule , module de la page d'accueil
+    path: 'home',
+    loadChildren: () => import("./modules/home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: 'inscription',
+    component: InscriptionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
