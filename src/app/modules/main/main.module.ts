d96ade24402471563fd063850af88a64c2a8583b
ElSombrero <nirilala15@gmail.com>
2020-06-25 22:26:25 +0300

    Ajout d'un system de postage de blog

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -7,17 +7,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainRoutingModule } from './main-routing.module';
 import { PostsComponent } from './components/posts/posts.component';
 import { ArticlesComponent } from './components/articles/articles.component';
+import { FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 
 @NgModule({
   declarations: [HomeComponent, NavbarComponent, FooterComponent, PostsComponent, ArticlesComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   exports:[
     HomeComponent,
     NavbarComponent,
     FooterComponent,
     RouterModule,
-    MainRoutingModule
-  ]
+    MainRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
 })
 export class MainModule { }
