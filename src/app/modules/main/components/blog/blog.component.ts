acaab9cd96e299830c05561274928a9d45e9618b
ElSombrero <nirilala15@gmail.com>
2020-07-06 10:02:02 +0300

    Nouveau design

--- src/app/modules/main/components/blog/blog.component.ts
+++ src/app/modules/main/components/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import* as M from "materialize-css/dist/js/materialize.min.js";
 
 @Component({
   selector: 'app-blog',
@@ -10,6 +11,7 @@ export class BlogComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    M.Materialbox.init(document.querySelectorAll('.materialboxed'));
   }
 
 }
